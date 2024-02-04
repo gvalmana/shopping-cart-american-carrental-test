48a9e58e2bcde257090e259ca48483cccc97d09c
Gustavo Valmaña <gustavo.shotokai@gmail.com>
2024-02-04 12:14:13 -0500

    agregado el producto

---
+++ app/Models/products/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models\products;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'code',
+        'price',
+    ];
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
